2e6c5a1449a321b233df0b7fd4b3fb25b850f484
Alexander Booth <adbubc+1@gmail.com>
2017-02-08 11:44:21 -0800

    Resolving some warnings.

--- bcsmssa/stats/models.py
+++ bcsmssa/stats/models.py
@@ -1,20 +1,21 @@
+from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
 
 # Create your models here.
 
 class Client ( models.Model ):
     client_number = models.IntegerField()
-    date_of_birth = models.CommaSeparatedIntegerField( max_length = 3 )
+    date_of_birth = models.CharField( max_length = 3, validators=[validate_comma_separated_integer_list])
     age = models.IntegerField()
     number_of_abuses = models.IntegerField()
-    services_required = models.CommaSeparatedIntegerField( max_length= 4 )
+    services_required = models.CharField( max_length = 4, validators=[validate_comma_separated_integer_list])
 
 class Abuse ( models.Model ):
-    start_date = models.CommaSeparatedIntegerField( max_length = 3 )
-    stop_date = models.CommaSeparatedIntegerField( max_length = 3 )
+    start_date = models.CharField( max_length = 3, validators=[validate_comma_separated_integer_list])
+    stop_date = models.CharField( max_length = 3, validators=[validate_comma_separated_integer_list] )
     role_of_abuser = models.IntegerField()
-    reported_date = models.CommaSeparatedIntegerField( max_length = 3 )
-    family_context = models.CommaSeparatedIntegerField( max_length = 12 )
+    reported_date = models.CharField( max_length = 3, validators=[validate_comma_separated_integer_list] )
+    family_context = models.CharField( max_length = 12, validators=[validate_comma_separated_integer_list] )
     client = models.ForeignKey( Client, on_delete = models.CASCADE )
 
 class Client_Current_Situation ( models.Model ):
@@ -28,5 +29,3 @@ class Client_Current_Situation ( models.Model ):
     profession = models.CharField(max_length=50)
     in_treatment = models.BooleanField()
     abuse = models.ForeignKey( Abuse, on_delete=models.CASCADE)
-
-

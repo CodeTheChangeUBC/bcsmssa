05fcae8730414740ff52eb69da1bf9d62992b2de
XIAOXIN YU <xiaoxiny@eos.ubc.ca>
2017-02-13 21:45:47 -0800

    generated db schema

--- bcsmssa/bcsmssa/settings.py
+++ bcsmssa/bcsmssa/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'stats',
+    'stats.apps.StatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

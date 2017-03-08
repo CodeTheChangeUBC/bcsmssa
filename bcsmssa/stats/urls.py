f24aa66dee6c3cf9887fc420515741b0d6b02e47
XIAOXIN YU <xiaoxiny@eos.ubc.ca>
2017-03-07 20:37:10 -0800

    fixed merge problems

--- bcsmssa/stats/urls.py
+++ bcsmssa/stats/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     url(r'^register/$', views.user_register, name='user_registration'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^statistics/$', views.statistics, name='statistics'),
-<<<<<<< HEAD
     url(r'^form/?$', views.form, name='form'),
-=======
-    url(r'^form/$', views.form, name='form'),
->>>>>>> cindyyu
     url(r'^invite_key$', views.invite_key),
 ]
